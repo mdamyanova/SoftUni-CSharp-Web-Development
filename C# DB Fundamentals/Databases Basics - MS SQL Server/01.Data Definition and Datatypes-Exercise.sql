ccad10969c68e23e84bc99e5be4228813324cc74
Mirela Damyanova <mdamyanova181@gmail.com>
2017-01-22 16:55:47 +0200

    edit

--- C# DB Fundamentals/Databases Basics - MS SQL Server/01.Data Definition and Datatypes-Exercise.sql
+++ C# DB Fundamentals/Databases Basics - MS SQL Server/01.Data Definition and Datatypes-Exercise.sql
@@ -384,7 +384,7 @@ CREATE TABLE Employees
 	DepartmentId int NOT NULL,
 	HireDate DATE NOT NULL,
 	Salary float NOT NULL,
-	AddressId int NOT NULL
+	AddressId int
 )
 
 ALTER TABLE Addresses
@@ -403,14 +403,56 @@ REFERENCES Addresses(Id)
 
 -- 18. Basic Insert 
 
+INSERT INTO Towns(Name) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas')
+
+INSERT INTO Departments(Name) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance')
+
+INSERT INTO Employees(FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary, AddressId) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00, NULL),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00, NULL),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, NULL),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00, NULL),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, NULL)
+
 -- 19. Basic Select all fields
+
+SELECT * FROM Towns
+SELECT * FROM Departments
+SELECT * FROM Employees
 
 -- 20. Basic Select all fields and order them
 
+SELECT * FROM Towns ORDER BY Name ASC
+SELECT * FROM Departments ORDER BY Name ASC
+SELECT * FROM Employees ORDER BY Salary DESC
+
 -- 21. Basic select some fields 
+
+SELECT Name FROM Towns ORDER BY Name ASC
+SELECT Name FROM Departments ORDER BY Name ASC
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees ORDER BY Salary DESC
 
 -- 22. Increase employees salary
 
+UPDATE Employees
+SET Salary += Salary * 0.1
+SELECT Salary FROM Employees
+
 -- 23. Decrease tax rate 
 
+UPDATE Payments 
+SET TaxRate -= TaxRate * 0.03
+SELECT TaxRate FROM Payments
+
 -- 24. Delete all records
+
+TRUNCATE TABLE Occupancies

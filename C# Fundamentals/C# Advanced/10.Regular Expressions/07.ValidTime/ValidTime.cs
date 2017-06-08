db081f4cadfc3971f579297756ce9f73f68471ab
mdamyanova <mdamyanova181@gmail.com>
2017-06-08 20:49:22 +0300

    edit

--- C# Fundamentals/C# Advanced/10.Regular Expressions/07.ValidTime/ValidTime.cs
+++ C# Fundamentals/C# Advanced/10.Regular Expressions/07.ValidTime/ValidTime.cs
@@ -7,51 +7,16 @@ namespace _07.ValidTime
     {
         public static void Main()
         {
-            var line = Console.ReadLine();
-            var regex = new Regex(@"^([01][0-9]):([012345][0-9]):([0123456][0-9]) [AP]M$");
+            var regex = new Regex(@"\b(0?[0-9]|1[0-2])(:[0-5]?[0-9]){2}\s?(A|P)M\b");
 
-            while (line != "END")
+            var time = Console.ReadLine();
+
+            while (time != "END")
             {
-                var match = regex.Match(line);
+                Console.WriteLine(regex.IsMatch(time) ? "valid" : "invalid");
 
-                if (match.Success)
-                {
-                    if (IsValidTime(match))
-                    {
-                        Console.WriteLine("valid");
-                    }
-                    else
-                    {
-                        Console.WriteLine("invalid");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("invalid");
-                }
-
-                line = Console.ReadLine();
+                time = Console.ReadLine();
             }
-        }
-
-        private static bool IsValidTime(Match clock)
-        {
-            var hours = int.Parse(clock.Groups[1].Value);
-            var minutes = int.Parse(clock.Groups[2].Value);
-            var seconds = int.Parse(clock.Groups[3].Value);
-
-            if (hours >= 0 && hours < 12)
-            {
-                if (minutes >= 0 && minutes < 60)
-                {
-                    if (seconds >= 0 && seconds < 60)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
         }
     }
 }

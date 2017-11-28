606aea7d542089e0332ebe045dcb28f7b4fbb2cc
mdamyanova <mdamyanova181@gmail.com>
2017-11-28 02:55:42 +0200

    working on

--- 09.C# Web/09.02.C# MVC Frameworks - ASP.NET Core/05.ASP.NET Core - The Right Way-Architecture - Exercise/LearningSystem/Areas/Admin/Controllers/CoursesController.cs
+++ 09.C# Web/09.02.C# MVC Frameworks - ASP.NET Core/05.ASP.NET Core - The Right Way-Architecture - Exercise/LearningSystem/Areas/Admin/Controllers/CoursesController.cs
@@ -1,9 +1,39 @@
 ﻿namespace LearningSystem.Web.Areas.Admin.Controllers
-{ 
+{
+    using Data.Models;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Models.Courses;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     public class CoursesController : BaseAdminController
     {
-        public IActionResult Create() => this.View();
+        private readonly UserManager<User> userManager;
+
+        public CoursesController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var trainers = await this.userManager
+                .GetUsersInRoleAsync(WebConstants.TrainerRole);
+
+            var trainerListItems = trainers
+                .Select(t => new SelectListItem
+                {
+                    Text = t.UserName,
+                    Value = t.Id
+                })
+                .ToList();
+
+            return this.View(new AddCourseFormModel
+            {
+                Trainers = trainerListItems
+            });
+        }
     }
 }
--- 09.C# Web/09.02.C# MVC Frameworks - ASP.NET Core/05.ASP.NET Core - The Right Way-Architecture - Exercise/LearningSystem/Areas/Admin/Models/Courses/AddCourseFormModel.cs
+++ 09.C# Web/09.02.C# MVC Frameworks - ASP.NET Core/05.ASP.NET Core - The Right Way-Architecture - Exercise/LearningSystem/Areas/Admin/Models/Courses/AddCourseFormModel.cs
@@ -1,6 +1,44 @@
 ﻿namespace LearningSystem.Web.Areas.Admin.Models.Courses
 {
-    public class AddCourseFormModel
+    using Data;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddCourseFormModel : IValidatableObject
     {
+        [Required]
+        [MaxLength(DataConstants.CourseNameMaxLenght)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.CourseDescriptionMaxLenght)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name="Trainer")]
+        [Required]
+        public string TrainerId { get; set; }
+
+        public IEnumerable<SelectListItem> Trainers { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(this.StartDate < DateTime.UtcNow)
+            {
+                yield return new ValidationResult("Start date should be in the future.");
+            }
+
+            if(this.StartDate > this.EndDate)
+            {
+                yield return new ValidationResult("Start date should be before end date.");
+            }
+        }
     }
 }

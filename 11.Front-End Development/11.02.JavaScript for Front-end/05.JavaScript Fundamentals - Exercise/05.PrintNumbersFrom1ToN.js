7e8a3b3d46115efb8fc6ce7d5e6cfb4c869abf7b
mdamyanova <mdamyanova181@gmail.com>
2018-04-11 20:12:44 +0300

    edit

--- 11.Front-End Development/11.02.JavaScript for Front-end/05.JavaScript Fundamentals - Exercise/05.PrintNumbersFrom1ToN.js
+++
@@ -1,9 +0,0 @@
-function printNumbers(n) {
-    for (let i = 1; i <= n; i++){
-        console.log(i);
-    }
-}
-
-printNumbers(5);
-console.log('---');
-printNumbers(2);
--- 11.Front-End Development/11.02.JavaScript for Front-end/05.JavaScript Fundamentals - Exercise/06.PrintNumbersFromNTo1.js
+++
@@ -1,9 +0,0 @@
-function printNumbers(n) {
-    for (let i = n; i >= 1; i--){
-        console.log(i);
-    }
-}
-
-printNumbers(5);
-console.log('---');
-printNumbers(2);
